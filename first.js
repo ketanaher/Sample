1b22c13950dd51a5710088bfeca1051bfa2d52c0
ketanaher <aherketan1302@gmail.com>
2025-11-11 22:58:19 +0530

    added resolve

--- first.js
+++ first.js
@@ -4,4 +4,4 @@ console.log("This is hello from ketan");
 
 console.log("Hello world");
 
-console.log("this is the sample of merge"); 
+console.log("this is the sample of merge it must be resolved"); 
